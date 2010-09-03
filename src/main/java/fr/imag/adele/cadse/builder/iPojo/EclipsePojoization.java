7955b6f2defcb242850dc263e3b1216522bcb346
Stephane Chomat <Stephane.chomat@imag.fr>
2010-09-03 15:15:36 +0200

    use unmodifed (only visibility) ipojo Pojoization

--- src/main/java/fr/imag/adele/cadse/builder/iPojo/EclipsePojoization.java
+++ src/main/java/fr/imag/adele/cadse/builder/iPojo/EclipsePojoization.java
@@ -1,3 +1,21 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package fr.imag.adele.cadse.builder.iPojo;
 
 import java.io.ByteArrayInputStream;
@@ -30,6 +48,7 @@ import java.util.jar.Manifest;
 import org.apache.felix.ipojo.manipulation.InnerClassManipulator;
 import org.apache.felix.ipojo.manipulation.Manipulator;
 import org.apache.felix.ipojo.manipulation.annotations.MetadataCollector;
+import org.apache.felix.ipojo.manipulator.Pojoization;
 import org.apache.felix.ipojo.manipulator.QuotedTokenizer;
 import org.apache.felix.ipojo.metadata.Attribute;
 import org.apache.felix.ipojo.metadata.Element;
@@ -61,7 +80,7 @@ class EclipsePojoization extends Pojoization {
      * Manipulate the input directory.
      * @throws CoreException 
      */
-    protected void manipulateDirectory() throws CoreException {
+    protected void manipulateDirectory() {
         manipulateComponents(); // Manipulate classes
         m_referredPackages = getReferredPackages();
         Manifest mf = doManifest(); // Compute the manifest
